65bc334b6725d3c52ee3d369a875dbd33e834076
Rasmus <rolfen@gmail.com>
2023-10-25 09:02:40 +0200

    Disaheim added

---
+++ Disaheim/Disaheim/Amulet.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disaheim
+{
+    public class Amulet
+
+    {
+        public string ItemId;
+        public string Design;
+        public Level Quality;
+
+        public Amulet(string itemId)
+        {
+            this.ItemId = itemId;
+            this.Quality = Level.medium;
+        }
+        public Amulet(string itemId, Level quality): this(itemId)
+        {
+            this.Quality= quality;
+        }
+        public Amulet(string itemId, Level quality, string design): this(itemId, quality)
+        {
+            this.Design = design;
+        }
+        
+
+        
+       
+        
+        public override string ToString()
+        {
+            return $"ItemId: {ItemId}, Quality: {Quality}, Design: {Design}";
+        }
+    }
+}
+    
+
+
+
+
---
+++ Disaheim/Disaheim/Book.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Disaheim
+{
+    public class Book
+    {
+        public string ItemId;
+        public string Title;
+        public double Price;
+
+        public Book(string itemID)
+        {
+            ItemId = itemID;
+        }
+        public Book(string itemID, string title) : this(itemID)
+        {
+            Title = title;
+        }
+        public Book(string itemID, string title, double price) : this(itemID, title)
+        {
+            Price = price;
+        }
+        public override string ToString()
+        {
+            return $"ItemId: {ItemId}, Title: {Title}, Price: {Price}";
+        }
+    }
+}
