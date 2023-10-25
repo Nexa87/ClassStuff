4e822e42c95e7b25ca275d37f7564510c6eb1874
Rasmus <rolfen@gmail.com>
2023-10-25 12:12:34 +0200

    Update til disa3

--- Disaheim/Disaheim/Utility.cs
+++ Disaheim/Disaheim/Utility.cs
@@ -1,12 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Disaheim
 {
-    internal class Utility
+    public class Utility
     {
+        public double GetValueOfBook(Book book)
+        {
+            return book.Price;
+
+        }
+        public double GetValueOfAmulet(Amulet amulet)
+        {
+            switch (amulet.Quality)
+            {
+                case Level.low:
+                    return 12.5;
+
+                    break;
+
+                case Level.medium:
+                    return 20.0;
+
+                    break;
+
+                case Level.high:
+                    return 27.5;
+
+                    break;
+                default:
+                    return 0;
+                    break;
+
+            }
+        }
+        
+
+        
+
     }
 }
